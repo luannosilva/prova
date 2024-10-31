ded484b86427d870ec3dee78ed45e933512819f1
luannosilva <luano250@gmail.com>
2024-10-30 22:23:09 -0300

    luan

---
+++ inicio.java
@@ -0,0 +1,11 @@
+inicio
+
+   1 "Acordo"
+   2 "Levanto da cama"
+   3 "Abro a porta do quarto"
+   4 "Ando até a porta do banheiro"
+   5 "Abro a porta do banheiro"
+   6 "Pego a escova de dentes"
+   7 "Escovo os dentes"
+   8 "Troco de roupa"
+   9 "Vou trabalhar"
